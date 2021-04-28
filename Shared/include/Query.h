a9ea0d4884b5f2b87cadfd8734897296ea17f469
Adam Giraudet <adamgiraudet@gmail.com>
2021-04-28 19:08:42 +0100

    Added shared query file

---
+++ Shared/include/Query.h
@@ -0,0 +1,129 @@
+#include <unordered_map>
+#include <string>
+#include "curl/curl.h"
+#include <algorithm>
+#include "uwebsockets/App.h"
+
+namespace //Anonymous namespace to only allow query and body to see/use this function
+{
+    std::unordered_map<std::string, std::string> parseURLValues(std::string_view source)
+    {
+        std::unordered_map<std::string, std::string> ret;
+        auto it = source.cbegin();
+        while (it != source.cend())
+        {
+            //We implicitly allow this to not find anything, indicating it is the last element
+            auto end = std::find(it, source.cend(), '&');
+
+            auto div = std::find(it, end, '=');
+
+            std::string id{ &*it, static_cast<size_t>(std::distance(it, div)) };
+            //Empty values are allowed, but empty names are not
+            if (div != end && std::distance(div, end) - 1 != 0)
+            {
+                std::string temp{ div + 1, end };
+                std::replace(temp.begin(), temp.end(), '+', ' '); //Spaces are encoded as '+' in queries (but not URLs, because consistency)
+                int curl_str_len = 0;
+                auto curl_str = curl_easy_unescape(nullptr, temp.data(), temp.size(), &curl_str_len);
+                ret[id].assign(curl_str, curl_str_len);
+                curl_free(curl_str);
+            }
+            else
+                ret[id] = "";
+
+            it = end;
+            if (it != source.cend())
+                ++it;
+        }
+        return ret;
+    }
+}
+
+class query
+{
+    std::unordered_map<std::string, std::string> elements;
+
+public:
+    query() = default;
+    query(uWS::HttpRequest* req) : elements(parseURLValues(req->getQuery())) {}
+
+    bool hasElement(const std::string& name, bool allowEmpty = false) const
+    {
+        if (!allowEmpty)
+            return elements.count(name) != 0 && !elements.at(name).empty();
+        else
+            return elements.count(name) != 0;
+    }
+    std::string_view getElement(const std::string& name) const
+    {
+        return elements.at(name);
+    }
+
+    bool containsAll(const std::vector<std::string>& strings, bool allowEmpty = false) const
+    {
+        for (const auto i : strings)
+        {
+            if (!hasElement(i, allowEmpty))
+                return false;
+        }
+        return true;
+    }
+
+    bool containsAny(const std::vector<std::string>& strings, bool allowEmpty = false) const
+    {
+        for (const auto i : strings)
+        {
+            if (hasElement(i, allowEmpty))
+                return true;
+        }
+        return false;
+    }
+    auto begin() const { return elements.cbegin(); }
+    auto end() const { return elements.cend(); }
+};
+
+class body
+{
+    std::unordered_map<std::string, std::string> elements;
+
+public:
+
+    body() = default;
+    body(std::string_view contents) : elements(parseURLValues(contents)) {}
+
+    bool hasElement(const std::string& name, bool allowEmpty = false) const
+    {
+        if (allowEmpty)
+            return elements.count(name) != 0 && !elements.at(name).empty();
+        else
+            return elements.count(name) != 0;
+    }
+    std::string_view getElement(const std::string& name) const
+    {
+        return elements.at(name);
+    }
+
+    bool containsAll(const std::vector<std::string>& strings, bool allowEmpty = false) const
+    {
+        for (const auto i : strings)
+        {
+            if (!hasElement(i, allowEmpty))
+                return false;
+        }
+        return true;
+    }
+
+    bool containsAny(const std::vector<std::string>& strings, bool allowEmpty = false) const
+    {
+        for (const auto i : strings)
+        {
+            if (hasElement(i, allowEmpty))
+                return true;
+        }
+        return false;
+    }
+
+
+    auto begin() const { return elements.cbegin(); }
+    auto end() const { return elements.cend(); }
+};
